3187b4c0025a950592d794bddf8447557986afe8
well300 <90952986+well300@users.noreply.github.com>
2021-09-22 13:27:59 +0530

    Update Hi.ts

--- src/commands/Misc/Hi.ts
+++ src/commands/Misc/Hi.ts
@@ -1,9 +1,9 @@
 import { MessageType } from '@adiwajshing/baileys'
+import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { IPackage, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
@@ -11,29 +11,19 @@ export default class Command extends BaseCommand {
             command: 'hi',
             description: 'Well....',
             category: 'misc',
-            usage: `${client.config.prefix}hi`,
+            usage: `${client.config.prefix}hi`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        
-        
-        
-return void M.reply(await request.buffer('https://i.pinimg.com/originals/be/f2/29/bef229a7ce4832a802fef81101dbd971.jpg'),
-MessageType.image,
-            undefined,
-            undefined,
-            `✇ Hey there It's Kakashi😆 how you doing..? `
-
-                    
-)
-                break
-            } catch (e) {
-                console.log(e)
-            }
-        }
-        return void null
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const image = this.client.assets.get('Kakashi')
+        if (!image) return void null
+        return void M.reply(await request.buffer('https://wallpapercave.com/uwp/uwp1426745.png'),
+        MessageType.image,
+                    undefined,
+                    undefined,
+            `✇ Hey there It's Kakashi😆 how you doing..?`
+        )
     }
 }
-
-                  
