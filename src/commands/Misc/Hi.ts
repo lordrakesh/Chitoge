dbdbdc27cb980b6d7bf5454483a6ea6835572212
well300 <90952986+well300@users.noreply.github.com>
2021-09-23 13:38:33 +0530

    Update Hi.ts

--- src/commands/Misc/Hi.ts
+++ src/commands/Misc/Hi.ts
@@ -1,30 +1,47 @@
-import { MessageType } from '@adiwajshing/baileys'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
 import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { IPackage, ISimplifiedMessage } from '../../typings'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'hi',
-            description: 'Well....',
+            description: 'Say hi to the bot.',
             category: 'misc',
             usage: `${client.config.prefix}hi`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const image = this.client.assets.get('chitoge')
-        if (!image) return void null
-        return void M.reply(
-            image,
-            MessageType.image,
-            undefined,
-            undefined,
-            `✇ Hey there It's Kakashi😆 how you doing..?`
+        const n = [
+            './assets/videos/chitoge/chitoge-hi.mp4'
+        ]
+        let chitoge = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `✇ Hey there It's Kakashi😆 how you doing..? \n` }
         )
     }
 }
+          
+       
+
+
+    
+        
+           
+           
+            
+            
+        
+    
+
+    
+        
+           
+           
+           
+   
