5bd544d94a01c3c3ebc4c9e860be5af5748f77f3
well300 <90952986+well300@users.noreply.github.com>
2021-09-23 19:33:51 +0530

    Update do_you_mean.ts

--- src/commands/General/do_you_mean.ts
+++ src/commands/General/do_you_mean.ts
@@ -1,7 +1,7 @@
-import { MessageType } from '@adiwajshing/baileys'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
 
@@ -9,34 +9,36 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: '',
-         
-
         })
     }
 
-
-
     run = async (M: ISimplifiedMessage): Promise<void> => {
-
-
-
-
-return void M.reply(await request.buffer('https://wallpapercave.com/uwp/uwp1426745.png'),
-MessageType.image,
-            undefined,
-            undefined,
-            `*No such command! BAKA-*`
-
-
-)
-
-
+        const n = [
+            './assets/videos/chitoge/chitoge-1.mp4'
+        ]
+        let chitoge = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `Do you mean *:help*? \n` }
+        )
     }
-
-
-
-
-
-
-
 }
+          
+       
+
+
+    
+        
+           
+           
+            
+            
+        
+    
+
+    
+        
+           
+           
+           
+   
