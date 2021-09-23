305158ed04b87e65109f9bcfa00b612ee3397e1c
well300 <90952986+well300@users.noreply.github.com>
2021-09-23 14:53:51 +0530

    Update Kakashi.ts

--- src/commands/Misc/Kakashi.ts
+++ src/commands/Misc/Kakashi.ts
@@ -1,41 +1,47 @@
-import { MessageType } from '@adiwajshing/baileys'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'Kakashi',
-            description: 'Displays the info',
+            command: 'chitoge',
+            description: 'Displays the info.',
             category: 'misc',
-            usage: `${client.config.prefix}chitoge`,
+            usage: `${client.config.prefix}chitoge`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        
-
-        
-        
-return void M.reply(await request.buffer('https://i.pinimg.com/736x/dc/b1/91/dcb191b5d7b12d20befcb034050c0f69.jpg'),
-MessageType.image,
-            undefined,
-            undefined,
-            `✇ *Kakashi* 〽 \n\n🍀 *Description:* Use *!mods* Command for repo or get owner number`
-           
-
-)
-
-
+        const n = [
+            './assets/videos/chitoge/chitoge-git.mp4'
+        ]
+        let chitoge = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `✇ *Kakashi* 〽 \n\n🍀 *Description:* Use *!mods* Command for repo or get owner numbe\n` }
+        )
     }
-
-
-
-
-
-
-
 }
+          
+       
+
+
+    
+        
+           
+           
+            
+            
+        
+    
+
+    
+        
+           
+           
+           
+   
