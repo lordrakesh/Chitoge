5b04a0c71a7597427070f7c1490f5de7bfee22af
well300 <90952986+well300@users.noreply.github.com>
2021-09-23 15:03:06 +0530

    Update do_you_mean.ts

--- src/commands/General/do_you_mean.ts
+++ src/commands/General/do_you_mean.ts
@@ -21,11 +21,11 @@ export default class Command extends BaseCommand {
 
 
 
-return void M.reply(await request.buffer('https://i.postimg.cc/DZBKfjpd/8debcf6fa25b3d81cd7896f61e4aea45-removebg-preview.png'),
+return void M.reply(await request.buffer('https://wallpapercave.com/uwp/uwp1426745.png'),
 MessageType.image,
             undefined,
             undefined,
-            `*No such command! Aah-*`
+            `*No such command! BAKA-*`
 
 
 )
