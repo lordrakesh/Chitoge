547c41275352d2bb3c36d8516bdc42ab04dd6329
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-09-26 22:27:58 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -78,8 +78,8 @@ export default class Command extends BaseCommand {
             })()
             return {
                 categories,
-                pack: pack[1] || '🎀𝙗𝙚𝙡𝙤𝙣𝙜𝙨 𝙩𝙤',
-                author: pack[2] || ' 𝙠𝙖𝙬𝙖𝙞𝙞 𝙗𝙖𝙠𝙖🎀',
+                pack: pack[1] || '𝑩𝒆𝒍𝒐𝒏𝒈𝒔 𝒕𝒐',
+                author: pack[2] || ' 𝑳𝒐𝒓𝒅 𝑹𝒂𝒌𝒆𝒔𝒉 ✇ ',
                 quality,
                 type: StickerTypes[
                     parsedArgs.flags.includes('--crop') || parsedArgs.flags.includes('--c')
