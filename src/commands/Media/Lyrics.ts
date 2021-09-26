c98d61f1c84c6df4defaa56c8cf9aa243d239ddf
well300 <90952986+well300@users.noreply.github.com>
2021-09-26 11:49:27 +0530

    Update Lyrics.ts

--- src/commands/Media/Lyrics.ts
+++ src/commands/Media/Lyrics.ts
@@ -35,7 +35,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `𓂀 𝓾𝓻𝓪𝓻𝓪𝓴𝓪 𓂀`,
+                        body: `🎀 𝙐𝙧𝙖𝙧𝙖𝙠𝙖 🎀`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
