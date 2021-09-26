bdf8265af15dd59e806e24285ef93de6285b3b39
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-09-26 22:16:01 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -36,204 +36,49 @@ export default class Command extends BaseCommand {
                 }
             }
             let text = `
-╭─「（＾∀＾●）ﾉｼ 」
-│❏ User: *${M.sender.username}*
-│❏ Name: 𝙐𝙧𝙖𝙧𝙖𝙠𝙖
-│❏ Prefix: !
-│❏ Owner: <!mod>
-╰────────────┈❥                 \n\n`
-            const keys = Object.keys(categories)
-            for (const key of keys)
-                text += `♡┈〚${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}〛┈♡\n• \`\`\`${categories[
-                    key
-                ]
-                    .map((command) => command.config?.command)
-                    .join(', ')}\`\`\`\n\n`
-            return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
-            mimetype: Mimetype.gif,
-            caption: `${text} 
- ──❁┈[ Uraraka Bᴏᴛ ]┈❁───
-
-┌────────────┈✽
-│   🎀 𝙐𝙧𝙖𝙧𝙖𝙠𝙖
-│   ©️ Synthesized Infinity Botto
-└────────────┈✽
-✽┈[Have Great Day]┈✽
-
-🎗 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
-            )
-        }
-        const key = parsedArgs.joined.toLowerCase()
-        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
-        if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
-        const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
-        M.reply(
-            `🚀 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
-                state ? 'Disabled' : 'Available'
-            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
-                command.config.aliases
-                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
-                    : ''
-            }\n🎐 *Group Only:* ${this.client.util.capitalize(
-                JSON.stringify(!command.config.dm ?? true)
-            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
-        )
-    }
-
-    emojis = ['🎈', '✨', '🎁', '🎵', '🌸', '💝', '💐', '🧨', '🎆']
-}
-import MessageHandler from '../../Handlers/MessageHandler'
-
-import BaseCommand from '../../lib/BaseCommand'
-
-import WAClient from '../../lib/WAClient'
-
-import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
-
-import { MessageType, Mimetype } from '@adiwajshing/baileys'
-
-import request from '../../lib/request'
-
-export default class Command extends BaseCommand {
-
-    constructor(client: WAClient, handler: MessageHandler) {
-
-        super(client, handler, {
-
-            command: 'help',
-
-            description: 'Displays the help menu or shows the info of the command provided',
-
-            category: 'general',
-
-            usage: `${client.config.prefix}help (command_name)`,
-
-            aliases: ['h']
-
-        })
-
-    }
-
-    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
-
-            const n = [
-
-            './assets/videos/chitoge/chitoge.mp4'
-
-        ]
-
-        let chitoge = n[Math.floor(Math.random() * n.length)]
-
-        if (!parsedArgs.joined) {
-
-            const commands = this.handler.commands.keys()
-
-            const categories: { [key: string]: ICommand[] } = {}
-
-            for (const command of commands) {
-
-                const info = this.handler.commands.get(command)
-
-                if (!command) continue
-
-                if (!info?.config?.category || info.config.category === 'dev') continue
-
-                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
-
-                else {
-
-                    categories[info.config.category] = []
-
-                    categories[info.config.category].push(info)
-
-                }
-
-            }
-
-            let text = `
-
 ╭─「 o((>ω< ))o 」
-
 │❏ User: *${M.sender.username}*
-
 │❏ Name: 𝖔𝖇𝖎𝖙𝖔
-
 │❏ Prefix: /
-
 │❏ Owner: </mod>
-
 ╰────────────┈☯                  \n\n`
-
             const keys = Object.keys(categories)
-
             for (const key of keys)
-
                 text += `✇┈〘${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}〙┈✇\n• \`\`\`${categories[
-
                     key
-
                 ]
-
                     .map((command) => command.config?.command)
-
                     .join(', ')}\`\`\`\n\n`
-
             return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
-
             mimetype: Mimetype.gif,
-
             caption: `${text} 
-
  ──☯┈[ 𝖔𝖇𝖎𝖙𝖔 𝖇𝖔𝖙 ]┈☯───
 
 ┌────────────┈☯
-
 │   ♠ 𝖔𝖇𝖎𝖙𝖔
-
 │   🏴 Simple Wa Bot
-
 └────────────┈☯
-
 ☯┈[Have Great Day]┈☯
 
 🧣 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
-
             )
-
         }
-
         const key = parsedArgs.joined.toLowerCase()
-
         const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
-
         if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
-
         const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
-
         M.reply(
-
             `🚀 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
-
                 state ? 'Disabled' : 'Available'
-
             }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
-
                 command.config.aliases
-
                     ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
-
                     : ''
-
             }\n🎐 *Group Only:* ${this.client.util.capitalize(
-
                 JSON.stringify(!command.config.dm ?? true)
-
             )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
-
         )
-
     }
 
     emojis = ['🖤', '☮', '🎁', '🎙', '🕸', '🎭', '🩹', '🧨', '♣']
-
 }
